6580572efdbd18db1c0cfa54b5d4226de931f2e2
Theo Lavole <theo.lavole@eleve.ensai.fr>
2016-09-23 10:14:31 +0200

    first commit

---
+++ src/main/java/maven/tp/App.java
@@ -0,0 +1,16 @@
+package maven.tp;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        /*System.out.println( "Hello World!" );*/
+        JOptionPane.showMessageDialog(null,"Hello World!");
+    }
+}
